7fd775dc4a0fcd11faa95ede02398ac580c7a500
yulia tikhonova <yuliatikhonova@outlook.com>
2020-05-06 19:29:13 -0700

    updated the lyric url and added 2 vars to go with it

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,8 @@
 //---------------------------Variables------------------------
 
-var lyricAPIKey = ''; //key for the lyric API
-var queryLyricURL = 'https://api.lyrics.ovh/v1/artist/title' + lyricAPIKey; //complete url for lyric API
+var queryLyricURL = 'https://api.lyrics.ovh/v1/' + artist + '/' + title; //complete url for lyric API
+var artist = $(#).value;
+var title = $(#).value;
 
 
 var artAPIKey = ''; //key for the album art API
